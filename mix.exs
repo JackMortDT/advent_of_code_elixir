c2ac6e0b5554be8e69531f23bc8167589b5f86bf
LuisSastré <soul6496@gmail.com>
2024-02-25 14:39:12 -0600

    Adding test dependencies

--- mix.exs
+++ mix.exs
@@ -7,22 +7,38 @@ defmodule AdventOfCode.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "AdventOfCode",
+      docs: docs(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.cobertura": :test
+      ]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.31", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.18", only: :test},
+      {:faker, "~> 0.17", only: :test}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "AdventOfCode",
+      extras: ["README.md"]
     ]
   end
 end
---
+++ test/advent_of_code/year2015/day1_test.exs
@@ -0,0 +1,9 @@
+defmodule AdventOfCode.Year2015.Day1Test do
+  use ExUnit.Case
+
+  alias AdventOfCode.Year2015.Day1
+  doctest Day1
+
+  test "" do
+  end
+end
